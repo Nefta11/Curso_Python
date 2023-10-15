d6098e544a4ca4ec1ba8beed7313d8ae64893e81
netf_ <neftaliarturohernandez@gmail.com>
2023-10-15 11:18:25 -0600

    Actualizacion Lesson one.

--- Lesson1/Variables.py
+++ Lesson1/Variables.py
@@ -13,3 +13,13 @@ numero2=88.9
 resultado=numero1+numero2
 print("La suma de ",numero1," + ",numero2," = ",resultado)
 
+print("--------------------------------------------------------------------------")
+print("--------------------------------------------------------------------------")
+print("Buscaremos la dirección de memoria en donde se encuentran almacenadas nuestras variables:")
+print("--------------------------------------------------------------------------")
+print("La variabre: ",miVariable, " se encuentra almacenada en la dirección de memoria: ", id(miVariable))
+print("La variabre: ",numero1, " se encuentra almacenada en la dirección de memoria: ", id(numero1))
+print("La variabre: ",numero2, " se encuentra almacenada en la dirección de memoria: ", id(numero2))
+print("La variabre: ",resultado, " se encuentra almacenada en la dirección de memoria: ", id(resultado))
+#Utilizamos ID() y dentro de la funcion colocamos nuestra variable, esto sirve para que nos muestra la dirección de memoria de nuestras variables
+#La dirección de variable no es constatnte y puede cambiar cada vez que se ejecuta el codigo ya que se almacena en la RAM
---
+++ usoDeVariables.py
@@ -0,0 +1,7 @@
+#Declaración de variables
+name="Neftali Arturo Hernández Vergara"
+numberTelephone=7641146446
+email="neftaliarturohernzndez@gmail.com"
+print(name)
+print(numberTelephone)
+print(email)
