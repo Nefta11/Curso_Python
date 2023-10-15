9fcc29f121d7ec09d1522cebc8f12d2e2e2a6cea
netf_ <neftaliarturohernandez@gmail.com>
2023-10-14 23:18:33 -0600

    Guardando ando...

---
+++ Lesson1/Variables.py
@@ -0,0 +1,15 @@
+miVariable="Hola Jovenes"
+
+print(miVariable)
+print(miVariable)
+
+miVariable="Hola Cambie la variable para demostrar que el codigo de ejecuta lineal"
+print(miVariable)
+
+print("-----------------------------------")
+print("Suma de dos números")
+numero1=100
+numero2=88.9
+resultado=numero1+numero2
+print("La suma de ",numero1," + ",numero2," = ",resultado)
+
