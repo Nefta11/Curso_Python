887425b5e7b30b1594a2eafa9ef1565c1e08ca3c
netf_ <neftaliarturohernandez@gmail.com>
2023-10-15 11:53:58 -0600

    Actualización Lesson 1

--- Lesson1/usoDeVariables.py
+++ Lesson1/usoDeVariables.py
@@ -1,7 +1,41 @@
 #Declaración de variables
+print("#Declaración de variables")
 name="Neftali Arturo Hernández Vergara"
 numberTelephone=7641146446
 email="neftaliarturohernzndez@gmail.com"
 print(name)
 print(numberTelephone)
 print(email)
+
+
+
+#Concatenación de Cadenas
+##En python para concatenar cadenas podemos hacerlo con  la ,
+
+print("#########################################################################################")
+print("##En python para concatenar cadenas podemos hacerlo con  la ,")
+print("#Concatenación de Cadenas")
+print("Mi nombre es :" , name , " Mi número de celular es: " , numberTelephone)
+print("Mi correo electronico es: ", email)
+
+
+
+#########################################################################################
+##Tipos de dato en Python
+print("#########################################################################################")
+print("##Tipos de dato en Python")
+#Tipo de dato String
+A="How are you?"
+print(type(A)) #Type es una funcion que te devuelve el tipo de dato que es la variable.
+
+##Tipo de dato Int
+B=7
+print(type(B))
+
+##Tipo de dato Float
+C=3.1416
+print(type(C))
+
+###Tipo de dato Boolean
+D=False
+print(type(D))
