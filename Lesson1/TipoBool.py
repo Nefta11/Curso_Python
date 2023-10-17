9d35dbd5caa6e52dc7e52f6899295f70a6a82396
Nefta11 <neftaliarturohernandez@gmail.com>
2023-10-17 10:53:29 -0600

    Ejercicio con boolean

---
+++ Lesson1/TipoBool.py
@@ -0,0 +1,12 @@
+#Declaración de variables tipo booleanas
+
+variable=False;
+print(variable)
+
+compararción= 10 == 10
+
+print(compararción)
+
+if compararción:
+    print("El resulado es verdadero")
+else: print("El resultado es falso")
