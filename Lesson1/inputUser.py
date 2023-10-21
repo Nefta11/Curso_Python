a209348fb66c5fd3a45e61919aeb3d63b7c84ecd
netf_ <neftaliarturohernandez@gmail.com>
2023-10-20 21:00:37 -0600

    actualizadoo...

--- Lesson1/inputUser.py
+++ Lesson1/inputUser.py
@@ -1,10 +1,13 @@
 #Función input para procesar la entrada de datos de usuarios 
 mensaje=input("Escribe un mesnsaje: ")
 
+#Por default cuando mandas un input en python espera recibir una cadena osea string
 print("El mensaje que has escrito es:",mensaje)
 print("Fin del programa.....")
 print("-----------------------------------------------------------------")
 
+
+#para que en un input se pueda meter un int o floar tienes que hacer como un tipo casteo, ejemplo: int(input("mensaje que quieras"))
 numero1 = int(input("Ingresa un número: "))
 numero2 = int(input("Ingresa un segundo número: "))
 multiplicación = numero1 * numero2
