ac8149d76792847e834311c53513b77c97478cb0
netf_ <neftaliarturohernandez@gmail.com>
2023-10-20 21:21:29 -0600

    Actualizado ado

--- Lesson1/Ejercicio.py
+++ Lesson1/Ejercicio.py
@@ -3,7 +3,7 @@ dia= int(input("Del 1 al 10, ¿Qué tal estuvo tu dia ?: "))
 print("Tu día estuvo: ",dia)
 
 ##Ejercicio 2, solicitar información de un libro a un usuario
-print("-------------------------------------------------------------")
+print("---------------------------------------------------------------")
 titulo=input("Titulo del libro: ")
 autor=input("Autor del libro:" )
 print("El libro",titulo,"fue escrito por",autor)
