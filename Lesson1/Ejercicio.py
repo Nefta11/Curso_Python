6b6379410b01da89585d2cb8c0f187f9b1d24ecf
netf_ <neftaliarturohernandez@gmail.com>
2023-10-20 21:16:54 -0600

    Actualizado...

---
+++ Lesson1/Ejercicio.py
@@ -0,0 +1,9 @@
+#Ejercicio- preguntarle al usuario como estuvo su dia utilizando int e input
+dia= int(input("Del 1 al 10, ¿Qué tal estuvo tu dia ?: "))
+print("Tu día estuvo: ",dia)
+
+##Ejercicio 2, solicitar información de un libro a un usuario
+print("-------------------------------------------------------------")
+titulo=input("Titulo del libro: ")
+autor=input("Autor del libro:" )
+print(titulo," fue escrito por ",autor)
