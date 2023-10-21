872856d91f7c02e4f15c4b81d2fed933aa0286af
netf_ <neftaliarturohernandez@gmail.com>
2023-10-20 20:52:58 -0600

    Actualizado...

--- Lesson1/inputUser.py
+++ Lesson1/inputUser.py
@@ -5,4 +5,7 @@ print("El mensaje que has escrito es:",mensaje)
 print("Fin del programa.....")
 print("-----------------------------------------------------------------")
 
-numero1=input
+numero1=input("Ingresa un número: ")
+numero2=input("Ingresa un segundo número: ")
+multiplicación=numero1 * numero2
+print("El resultado de la multiplicacion del número: ",numero1," * ",numero2," = ",multiplicación)
