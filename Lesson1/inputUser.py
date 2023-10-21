7e0b2368050e065b6936a5003c14aaaa47b2ac05
netf_ <neftaliarturohernandez@gmail.com>
2023-10-20 20:49:28 -0600

    Actualizado...

---
+++ Lesson1/inputUser.py
@@ -0,0 +1,8 @@
+#Función input para procesar la entrada de datos de usuarios 
+mensaje=input("Escribe un mesnsaje: ")
+
+print("El mensaje que has escrito es:",mensaje)
+print("Fin del programa.....")
+print("-----------------------------------------------------------------")
+
+numero1=input
