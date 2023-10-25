7ecc2500e479ee1967603fa9adf629e11cca6be4
Nefta11 <neftaliarturohernandez@gmail.com>
2023-10-24 21:31:30 -0600

    Actualizado..

---
+++ Lesson2/Operadores/operadores.py
@@ -0,0 +1,7 @@
+operandoA=3
+operandoB=2
+
+suma =operandoA + operandoB
+print("Resultado de la suma:",suma)
+#Otra manera de imprimir las varibales dentro de la misma candena 
+#Es con la interpolación que sirve para incluir variables dentro de una cadenar
