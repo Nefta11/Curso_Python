f08a87e24343b53cabe2c2b54399cbae7d27e318
Nefta11 <neftaliarturohernandez@gmail.com>
2023-10-24 21:58:36 -0600

    Actualizado...

--- Lesson2/OperadoresAritmeticos/ejercicioArea.py
+++ Lesson2/OperadoresAritmeticos/ejercicioArea.py
@@ -1,2 +1,16 @@
 #Ejercico calcular el área de un rectángulo
 print("Calcular el área de un rectángulo")
+print("-------------------------------------")
+alto=int(input("Proporciona el valor del alto de tu rectángulo:"))
+ancho=int(input("Proporciona el valor del ancho de tu rectángulo:"))
+
+perimetro=(alto+ancho)*2
+area= alto*ancho
+
+print("-------------------------------------")
+print(f"El valor del perimetro es: {perimetro}")
+print(f"El valor del área es: {area}")
+
+"""
+La
+"""
