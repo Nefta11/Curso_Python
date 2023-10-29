575582ef470d8583dab3c0601f7fac7a9f541ef2
Nefta11 <neftaliarturohernandez@gmail.com>
2023-10-29 15:03:41 -0600

    Siuuu

---
+++ Lesson2/OperadoresAritmeticos/operadorComparación.py
@@ -0,0 +1,25 @@
+#Operadores de comparacion
+a=4
+b=6
+#Operado =
+resultado = (a==b)
+print(f"Resultado de comparar {resultado}")
+
+resultado= a!=b
+print(f"Resultado !=: {resultado}")
+
+#Operado >
+resultado = a>b
+print(f"Resultado >: {resultado}")
+
+#operado >=
+resultado= a>=b
+print(f"Resultado >= {resultado}")
+
+#operador a < b
+resultado= a<b
+print(f"Resultado <: {resultado}")
+
+#operador <=
+resultado= a<=b
+print(f"Resultado <=: {resultado}")
