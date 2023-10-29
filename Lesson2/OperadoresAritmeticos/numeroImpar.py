658bbb5978f64fb0b5243e81614d1f5189da9fb3
Nefta11 <neftaliarturohernandez@gmail.com>
2023-10-29 15:12:55 -0600

    Actualizado...

---
+++ Lesson2/OperadoresAritmeticos/numeroImpar.py
@@ -0,0 +1,12 @@
+#Ejerecio verificar si un número ingresado por un usuario es par o impar
+
+print("Programa que te verifica si un número es par o impar")
+print("----------------------------------------------------")
+
+Numero=int(input("Ingresa un número:"))
+
+if Numero % 2 == 0:
+    print(f"El número {Numero} es par")
+else:
+    print(f"El número de {Numero} es impar")    
+    
