b69b93b7364b3518a0bcc4ccb5a6f2825354b89e
Nefta11 <neftaliarturohernandez@gmail.com>
2023-10-29 15:31:20 -0600

    Siuu

---
+++ Lesson2/OperadoresAritmeticos/OperadoresLogicos/operadorLogico.py
@@ -0,0 +1,4 @@
+#Operadores Logicos
+#and esta exprecion devuelve true si ambos valores son verdaderos en otro caso devulve falso
+#or esta exprecion dvuelve true si alguno de los valores son verdaderos en otro para devolver falso ambos valores deven ser falsos
+#not es un operador unario solo nececita de un valor para funcionar 
