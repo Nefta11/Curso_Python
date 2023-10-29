4eee6f708ca084bd460619bea0013593d9b9982c
Nefta11 <neftaliarturohernandez@gmail.com>
2023-10-29 15:51:59 -0600

    Siuu

---
+++ Lesson2/OperadoresAritmeticos/OperadoresLogicos/ejercicioOr.py
@@ -0,0 +1,11 @@
+#Ejercicio realizado con OR 
+#Ejercicio que determina si un padre puede asistir a un juego de su hijo dependiendo si 
+#Esta en su dia de descanso o vacaciones
+
+vacaciones=True
+diaLibre=False
+
+if vacaciones or diaLibre:
+    print("Adelante puedes asistir")
+else:
+    print("No puedes ir tienes deberes que hacer")
