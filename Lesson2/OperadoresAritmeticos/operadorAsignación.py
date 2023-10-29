fbc5b2936e0c19120ecda45f7f51e7be19cb5e75
Nefta11 <neftaliarturohernandez@gmail.com>
2023-10-29 14:10:48 -0600

    Actualizado...

---
+++ Lesson2/OperadoresAritmeticos/operadorAsignación.py
@@ -0,0 +1,18 @@
+miVariable=10
+
+print(miVariable)
+
+#Reasignación de variable 
+miVariable= miVariable+10
+print(miVariable)
+#Al hacer la reasignación ahora a la variable original le aumentara 10 
+#Por lo que el resultado sera 20
+
+#Incremento de Variable de manera mas reducida 
+#Uncremento con reasignación
+miVariable+=2
+print(f"Incremento con reasignación {miVariable}")
+
+#Restar un valor a mi variable
+miVariable-=5
+print(f"Mi variable con decremento {miVariable}")
