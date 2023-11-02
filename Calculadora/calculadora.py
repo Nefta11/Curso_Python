14e76f52f885cdfd146abc07fac471d8e725b80e
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-02 10:27:06 -0600

    Actualisado..

---
+++ Calculadora/calculadora.py
@@ -0,0 +1,15 @@
+n1= int(input())
+op=input()
+n2=int(input())
+
+if op == "+":
+    print(n1, op, n2, "=",(n1+n2))
+
+if op == "-":
+    print(n1, op, n2, "=",(n1-n2))
+
+if op == "*":
+    print(n1, op, n2, "=",(n1*n2))
+
+if op == "/":
+    print(n1, op, n2, "=",(n1/n2))
