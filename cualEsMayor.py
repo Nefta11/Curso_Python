858c02cd9d08c92f16b146200d284d992cd7e7bc
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-02 21:07:32 -0600

    Siuuu

---
+++ cualEsMayor.py
@@ -0,0 +1,12 @@
+#Ejercicio que determina cual número es mayor de dos 
+#Que un usuario mete
+
+print("Programa que determina que número es mayor de dos:")
+
+Num1=int(input("Inserte el primer número: "))
+Num2=int(input("Inserte el segundo número: "))
+
+if Num1>Num2:
+    print(f"El número mayor es: {Num1}")
+else:
+    print(f"El número mayor es: {Num2}")
