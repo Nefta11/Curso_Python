bb5878fe3227dc8791251543d6c5382db647d42e
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-02 21:37:12 -0600

    Listo siuuu

--- Lesson2/OperadoresAritmeticos/tiendaLibros.py
+++ Lesson2/OperadoresAritmeticos/tiendaLibros.py
@@ -5,17 +5,21 @@ name=input("Proporcione el nombre:")
 id=int(input("Proporcione el ID:"))
 price=float(input("Proporcione el precio:"))
 send=input("Indica si el envio es gratuito (True/False): ")
-
+##Siempre que metamo ya sea una cadena con algun caracter nos dara true al utilizar boll input
+##Y en su caso contrario nos dara false su mandamos una cadena vacia
+#Por lo que tenemos que hacer la comprobacion con if:
 if send == "True":
     send = True
 elif send =="False":
     send=False
 else:
     send= 'Valor incorrecto, debe escribir True/False'
-    
+
+
+##Utilizamo triple comilla simple para concatenar varias cadenas y variables 
 print("------------------------------------------------")
-print(f"Nombre:{name}")
-print(f"ID: {id}")
-print(f"Precio: {price}")
-print(f"Envio Gratuito: {send}")
+print(f'''Nombre:{name},
+ID: {id},
+Precio: {price},
+Envio Gratuito: {send}''')
 print("-----------------------------------------------")
