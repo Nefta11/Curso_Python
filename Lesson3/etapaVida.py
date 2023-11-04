9d99d6fd7df649433447c868208fd3a4fd05e1eb
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-03 21:54:57 -0600

    Siuu

--- Lesson3/etapaVida.py
+++ Lesson3/etapaVida.py
@@ -3,6 +3,12 @@ print("------------------------------------")
 edad= int(input("Proporciona tu edad: "))
 etapa=""
 
-if edad > 0 & edad < 10:
+if edad > 0 & edad <= 9:
     print(f"Tu edad es de {edad} \nLa infancia es increible vive al maximo bebe...")
-  
+elif edad >=10 & edad <= 19:
+    print(f"Tu edad es de {edad} \nMuchos cambios y mucho estudio...")
+elif edad >=20 & edad <= 30:
+    print(f"Tu edad es de {edad} \nAmor y comienza el trabajo...")
+else:
+    print("Etapa de vida no reconocida Bobito")
+
