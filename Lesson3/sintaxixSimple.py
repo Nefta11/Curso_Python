26244072bef553a826c2e77a9985b76a40447aa6
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-03 21:21:01 -0600

    Siuuu

---
+++ Lesson3/sintaxixSimple.py
@@ -0,0 +1,11 @@
+#Sintaxis simplificada con el operador ternario
+#Solo se recomienda para codigos muy pequeños 
+condicion = False 
+
+#if condicion:
+#   print("Condicion verdadera")
+#else:
+#   print("Condicion falsa")
+
+
+print("Condicion verdadera") if condicion else print("Condicion falsa")
