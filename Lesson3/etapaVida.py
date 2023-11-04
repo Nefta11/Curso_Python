a70019f5e09e180312d4626d9156418a701bc1ac
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-03 21:50:31 -0600

    Siuuu

---
+++ Lesson3/etapaVida.py
@@ -0,0 +1,8 @@
+#Programa que pide que proporciones tu edad y dependiendo de esta te dara un mensaje.
+print("------------------------------------")
+edad= int(input("Proporciona tu edad: "))
+etapa=""
+
+if edad > 0 & edad < 10:
+    print(f"Tu edad es de {edad} \nLa infancia es increible vive al maximo bebe...")
+  
