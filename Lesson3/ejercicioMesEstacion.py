e3bec227faa42de20434f738455fe1ee7d4bff8d
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-03 21:41:34 -0600

    Siuuu

--- Lesson3/ejercicioMesEstacion.py
+++ Lesson3/ejercicioMesEstacion.py
@@ -10,13 +10,17 @@ station= None
 
 if month == 3 or month== 4 or month == 5:
     station= "Invierno"
+    print(f"El mes:{month} esta dentro de la estación: {station} ")
 elif month == 6 or month== 7 or month== 8:
     station = "Verano"
+    print(f"El mes:{month} esta dentro de la estación: {station} ")
 elif month == 9 or month== 10 or month== 11:
     station="Otoño"
+    print(f"El mes:{month} esta dentro de la estación: {station} ")
 elif month == 12 or month== 1 or month== 2:
     station="Invierno"
+    print(f"El mes:{month} esta dentro de la estación: {station} ")
 else:
     print("Valor no valido Bro")
 
-    print(f"El mes:{month} esta dentro de la estación: {station} ")
+    
