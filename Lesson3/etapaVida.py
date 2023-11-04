51fcf567b73aa827134bc3645958ae1788774f95
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-03 22:03:04 -0600

    Siuuu

--- Lesson3/etapaVida.py
+++ Lesson3/etapaVida.py
@@ -1,13 +1,13 @@
 #Programa que pide que proporciones tu edad y dependiendo de esta te dara un mensaje.
 print("------------------------------------")
 edad= int(input("Proporciona tu edad: "))
-etapa=""
 
-if edad > 0 & edad <= 9:
+
+if 0 <= edad <10:
     print(f"Tu edad es de {edad} \nLa infancia es increible vive al maximo bebe...")
-elif edad >=10 & edad <= 19:
+elif 10<= edad < 20:
     print(f"Tu edad es de {edad} \nMuchos cambios y mucho estudio...")
-elif edad >=20 & edad <= 30:
+elif 20 <= edad < 30:
     print(f"Tu edad es de {edad} \nAmor y comienza el trabajo...")
 else:
     print("Etapa de vida no reconocida Bobito")
