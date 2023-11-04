fa69caa8352d64b2c6f7fde94ff74346c5e2792c
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-03 21:09:09 -0600

    Siuuu

---
+++ Lesson3/NumAtext.py
@@ -0,0 +1,10 @@
+numero= int(input("Proporciona un valor entre 1 y 3:"))
+
+if numero == 1:
+    print("Numero 1")
+elif numero == 2:
+    print("Numero 2")
+elif numero == 3:
+    print("Numero 3")
+else:
+    print("Numero fuera de rango bobito")
