d174739eb5e0bd05f81eb992b42816a9b451abb9
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-03 20:58:11 -0600

    siuuu

--- Lesson3/main.py
+++ Lesson3/main.py
@@ -1,6 +1,10 @@
 #Aqui comenzamos a ver las sentencia if/else
 
 condicion= False
+#En python no necesariamente tenemos que tener los valores flase y verdadero
+#Ya que por default una cadena que contenga algo lo detectara como true
+#y si la cadena esta vacia devuelve falso
+
 
 if condicion:
     print('condición verdadera')
