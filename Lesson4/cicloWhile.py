4dc5a1ee2f489649ad37b8808fc6aeb3982098f1
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-18 15:15:24 -0600

    Update

--- Lesson4/cicloWhile.py
+++ Lesson4/cicloWhile.py
@@ -1,6 +1,9 @@
-condicion = True
+#Inicialización del ciclo while
+#condicion = True
 
-while condicion:
-    print("Ejecutando ciclo while")
-else:
-    print("Fin ciclo while")
+#while condicion:
+#    print("Ejecutando ciclo while")
+#else:
+#    print("Fin ciclo while")
+
+
