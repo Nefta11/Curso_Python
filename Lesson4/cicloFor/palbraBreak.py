fdaa26e2b096fdaafce35416ffe41f6190d3a11f
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-18 15:55:48 -0600

    Update..

---
+++ Lesson4/cicloFor/palbraBreak.py
@@ -0,0 +1,8 @@
+#Palbra break dentro de los ciclos
+
+for letra in 'Holanda':
+    if letra == "a":
+        print(f"Letra encontrada: {letra}")
+        break
+    else:
+        print("Fin ciclo for") 
