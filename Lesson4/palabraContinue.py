0badcd4c9e50afbe26e932fe918241f8abc2e474
Nefta11 <neftaliarturohernandez@gmail.com>
2023-11-18 20:44:56 -0600

    Update...

--- Lesson4/palabraContinue.py
+++ Lesson4/palabraContinue.py
@@ -1,2 +1,3 @@
-for i in range(6):      #Funcion de range
-    print(f"Valor: {i}")
+#for i in range(6):#Funcion de range, le das un rango de nuemero y te da esa cantidad iniciando en 0
+ #   if i % 2 ==0: #De esta manera nos manda solo los numeros pares
+  #      print(f"Valor: {i}")
