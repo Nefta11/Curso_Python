626ee2557ab9d197c36ba2eccce1604e9d7e1cd9
Nefta11 <neftaliarturohernandez@gmail.com>
2023-12-13 12:43:23 -0600

    Update

--- Lesson5/flor.py
+++ Lesson5/flor.py
@@ -11,4 +11,10 @@ for i in range(16):
         color(c)
         h+=0.005
         rt(90)
-        
+        circle(150-j*6,90)
+        lt(90)
+        circle(150-j*6,90)
+        rt(180)
+    circle(40,24)
+
+done()
