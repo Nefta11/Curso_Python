39fb76729108710c065bfaaba78ca3531d3b47ba
Nefta11 <neftaliarturohernandez@gmail.com>
2023-12-15 14:18:01 -0600

    Update

--- Lesson5/lista2.py
+++ Lesson5/lista2.py
@@ -3,4 +3,4 @@
 nombre= ["Neftali","Arturo","Hernandez","Vergara"]
 
 #Imprimir un rango de la lista
-print(nombre[0:2])
+print(nombre[0:2]) #En este ejemplo mostramos como recuperamos e imprimimos un rango de lo que tenemos en nuestra lista de nombres en este caso el nombre que aparece en la posicion 0 y 2
