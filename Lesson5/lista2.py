0989289312b6b5a3c3d7103c9fde99c1df940728
Nefta11 <neftaliarturohernandez@gmail.com>
2023-12-16 12:07:11 -0600

    Update

--- Lesson5/lista2.py
+++ Lesson5/lista2.py
@@ -1,16 +1,22 @@
 #Acceder a un rango de nuestra lista
 
-nombre= ["Neftali","Arturo","Hernandez","Vergara"]
+nombres= ["Neftali","Arturo","Hernandez","Vergara"]
 
 #Imprimir un rango de la lista
-print(nombre[0:2]) #En este ejemplo mostramos como recuperamos e imprimimos un rango de lo que tenemos en nuestra lista de nombres en este caso el nombre que aparece en la posicion 0 y 2
+print(nombres[0:2]) #En este ejemplo mostramos como recuperamos e imprimimos un rango de lo que tenemos en nuestra lista de nombres en este caso el nombre que aparece en la posicion 0 y 2
 
 #Ir del inicio de la lista al indice (sin incluirlo )
-print(nombre[:3])
+print(nombres[:3])
 
 #Desde el indice indicado hasta el final
-print(nombre[1:])
+print(nombres[1:])
 
 #Cambiar un valor 
-nombre[3]="Avryl"
-print(nombre)
+nombres[3]="Avryl"
+print(f"Sustituyendo el elemendo en el indice 3",nombres)
+
+#Iterar una lista
+for nombre in nombres:
+    print(nombre)
+else:
+    print("No existen mas valores en la lista")
