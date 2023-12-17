6c55cc6c46935b05953b721d76d919b714602406
Nefta11 <neftaliarturohernandez@gmail.com>
2023-12-17 17:08:57 -0600

    Update

---
+++ Lesson5/tuplas2.py
@@ -0,0 +1,8 @@
+verduras =("Cebolla","Aguacate","Tomate")
+
+print(verduras)
+
+#recorre elementos
+
+for verdura in verduras:
+    print(verdura, end= " ")#Se imprime en una sola linea solo dejando un espacio en blanco
