ad6a48403f161448bf8459c25f1c75d1af309c74
Nefta11 <neftaliarturohernandez@gmail.com>
2023-12-17 16:44:15 -0600

    Update

---
+++ Lesson5/ejercicios/rangoDiv3.py
@@ -0,0 +1,18 @@
+#Ejercicio 1 
+#Iterar un rango de 0 a 10 e imprimir números divisibles entre 3
+print("Rango de 0 a 10 con numeros divisibles entre 3")
+for i in  range (11):
+    if(i%3 == 0):
+        print(i)
+
+
+#Ejercicio 2
+#Crear un rango de numeros de 2 a 6, e imprimirlos
+print("Rango de 2 a 6")  
+for i in range (2,7):
+    print(i)
+
+#Ejercicio 3
+#Crear un rango de 3 a 10, pero con incremento de 2 en 2 en lugar de 1
+
+while
