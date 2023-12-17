d9c5f68a4b3d2f777c9fb3b07f4ef396c762839e
Nefta11 <neftaliarturohernandez@gmail.com>
2023-12-17 17:28:08 -0600

    Update

--- Lesson5/ejercicioTuplas.py
+++ Lesson5/ejercicioTuplas.py
@@ -2,7 +2,9 @@
 #Crear una lista que solo incluya los números menores a 5
 
 tuplas =(12,1,8,3,2,5,8)
-
+lista =[]
 for tupla in tuplas:
-    if(tupla <5):
-        print(tupla)
+    if tupla <5:
+        lista.append(tupla)
+
+print(lista)
