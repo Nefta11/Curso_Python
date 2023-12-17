fc1dd0cf633394ce270167b425e0f47c5325979a
Nefta11 <neftaliarturohernandez@gmail.com>
2023-12-17 16:53:46 -0600

    Update

--- Lesson5/ejercicios/rangoDiv3.py
+++ Lesson5/ejercicios/rangoDiv3.py
@@ -14,5 +14,7 @@ for i in range (2,7):
 
 #Ejercicio 3
 #Crear un rango de 3 a 10, pero con incremento de 2 en 2 en lugar de 1
-
-while
+print("Rango con valores de inicio=3, fin=10, increento = 2")
+rango = range(3,11,2)
+for i in rango:
+    print(i)
