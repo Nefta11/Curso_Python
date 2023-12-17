55590f4c198662a1ee50275bbc71da7d3c0e417b
Nefta11 <neftaliarturohernandez@gmail.com>
2023-12-17 17:13:55 -0600

    Update

--- Lesson5/tuplas2.py
+++ Lesson5/tuplas2.py
@@ -6,3 +6,12 @@ print(verduras)
 
 for verdura in verduras:
     print(verdura, end= " ")#Se imprime en una sola linea solo dejando un espacio en blanco
+
+#Cambiar valor de una tupla
+
+#Conversion de tupla a lista para modificarla
+    
+verdurasLista= list(verduras)#Con esta funcion se convierte nuestra tupla a lista
+verdurasLista[0]="Zanahoria"
+verduras= tuple(verdurasLista) # Sirve para cambiarla a tupla nuevamente
+print("\n",verduras)
