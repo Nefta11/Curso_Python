296fc946946422d65e77967a282dbab26f42b5fd
Nefta11 <neftaliarturohernandez@gmail.com>
2023-12-19 19:08:43 -0600

    Update SET

--- Lesson5/set.py
+++ Lesson5/set.py
@@ -1,8 +1,11 @@
 #Coleccion de tipo SET
 
 #Se caracteriza por:
-#No mantiene un orden como la tupla y la lista
+#No mantiene un orden como la tupla y la lista(o tiene un indice)
 #No permite almacenar elementos duplicados
 #No es posible modificar los elemementos ya almacenados
 #Si es posible agregar o eliminar elementos 
 
+#Se utilizan llaves
+planetas = {"Marte","Jupiter","Venus"}
+print(planetas)
