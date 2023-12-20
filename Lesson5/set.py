cbdc6b4c81b381d1142164acabeb69d41bdf352e
Nefta11 <neftaliarturohernandez@gmail.com>
2023-12-19 19:06:19 -0600

    Add SET

---
+++ Lesson5/set.py
@@ -0,0 +1,8 @@
+#Coleccion de tipo SET
+
+#Se caracteriza por:
+#No mantiene un orden como la tupla y la lista
+#No permite almacenar elementos duplicados
+#No es posible modificar los elemementos ya almacenados
+#Si es posible agregar o eliminar elementos 
+
