19165339b3eb6a604b0454aaabcd328f1d75a5e7
Nefta11 <neftaliarturohernandez@gmail.com>
2023-12-20 19:41:26 -0600

    Add Update Dicc

--- Lesson5/diccionarios.py
+++ Lesson5/diccionarios.py
@@ -1,2 +1,14 @@
 #Diccionarios en PYTHON
+#(Se compone de una key y value)
 
+diccionario = {
+    'IDE':'Integrated Development Enviroment',
+    'OPP':'Object Oriented Programming',
+    'DBMS':'Database Management System'
+}
+
+print(diccionario)
+
+#Largo de elementos
+
+print(len(diccionario))
