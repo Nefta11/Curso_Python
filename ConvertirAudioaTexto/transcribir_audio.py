70b191f764102ad4337b579d393d7c1020236152
Nefta11 <neftaliarturohernandez@gmail.com>
2024-07-21 18:36:52 -0600

    Updated

--- ConvertirAudioaTexto/transcribir_audio.py
+++ ConvertirAudioaTexto/transcribir_audio.py
@@ -1,20 +1,13 @@
-from pydub import AudioSegment
 import speech_recognition as sr
 
-# Load the .opus file
-audio = AudioSegment.from_file("PTT-20240715-WA0007.opus")
-
-# Export the audio as .wav
-audio.export("audio.wav", format="wav")
-
-# Initialize recognizer
+# Inicializar el reconocedor
 recognizer = sr.Recognizer()
 
-# Load the audio file
-audio_file_path = "audio.wav"
+# Cargar el archivo de audio convertido
+audio_file_path = "PTT-20240715-WA00076.wav"  # Cambiar a "PTT-20240715-WA00076.wav"
 with sr.AudioFile(audio_file_path) as source:
     audio_data = recognizer.record(source)
 
-# Transcribe the audio
+# Transcribir el audio
 text = recognizer.recognize_google(audio_data, language="es-ES")
 print(text)
