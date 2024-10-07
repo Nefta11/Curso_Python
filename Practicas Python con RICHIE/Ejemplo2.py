722d8c219103b5279eeafa0736b206163e78ec18
Nefta11 <neftaliarturohernandez@gmail.com>
2024-10-07 16:12:17 -0600

    Updated add signo zodiacal

---
+++ Practicas Python con RICHIE/Ejemplo2.py
@@ -0,0 +1,13 @@
+#Ciclos If, FOR , WHILE , FOR EACH
+
+Edad = 25
+if Edad < 12:
+    print("Eres un niño")
+elif Edad < 11 and Edad>16:
+    print("Es un adolecente")
+elif Edad > 15 and Edad<17:
+    print("Es usted joven chii")
+elif Edad > 26 and Edad < 58:
+    print("Es adulto")
+else: 
+    print("Eres una vejete")
