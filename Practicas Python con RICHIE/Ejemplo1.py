c740c2bf751db2de5d4e878f5997bbe003a2264c
Nefta11 <neftaliarturohernandez@gmail.com>
2024-10-07 15:30:32 -0600

    Add carpeta Richie

---
+++ Practicas Python con RICHIE/Ejemplo1.py
@@ -0,0 +1,71 @@
+print("Hi welcome to python")
+
+##Definicion de vriables
+
+valor1 = 51
+valor2 = 10
+print ("El resultado de la suma es :")
+print(valor1+valor2)
+
+Edad = 1
+
+if Edad > 17:
+    print("Eres mayor de edad")
+else:
+    print("Es menor de edad usted tontito")
+
+
+Nombre = "Neftali"
+Altura = 1.68
+
+print("Nombre:", Nombre, "Altura :",Altura)
+
+
+###############################
+Mensaje = "El valor de la suma de A + B multiplicado por c es igual :"
+a,b,c =4,8,2
+c = a + b * c
+print(Mensaje,c)
+
+'''
+comentarios siuuu
+'''
+
+'''
+Identacion de un Tab es igual a 3 u 4 espacios
+'''
+
+g= 2+5+6+7+8+\
+1+2+2
+
+print(g)
+
+h=(1+2+3+3
++4+5+
+6+78)
+
+print(h)
+
+##Funciones en python
+
+def suma(a,b):
+    return a + b
+
+def resta(a,b):
+    return a-b
+
+def Multi(a,b):
+    return a * b
+
+def Div(a,b):
+    return a/b
+
+a= suma(22,12)
+b= resta(22,12)
+c= Multi(22,12)
+d= Div(22,12)
+print("El resultado de la suma es :",a)
+print("El resultado de la resta es :",b)
+print("El resultado de la Multi es :",c)
+print("El resultado de la Div es :",d)
+
