14c9e955b6a6418e1e3852117ae4e5b43ae03443
Nefta11 <neftaliarturohernandez@gmail.com>
2024-10-14 15:38:44 -0600

    Add function encender

---
+++ Practicas Python con RICHIE/ejemplo3.py
@@ -0,0 +1,37 @@
+#Funciones
+
+llave = False
+
+
+def avanzar():
+    print ("avanzar, run, ruun")
+    
+def frenar():
+    print ("frenar, parar, parkear")
+
+def retroceder():
+        print ("retroceder, reversa, reversa")
+
+def pitar():
+     print ("pitar, gritar, gritar")  
+
+def encender():
+     print ("colocar llave")
+     llave= True
+     if (llave):
+        print("carro encendido")
+
+def apagar():
+     print ("quitar llave")
+     llave= False
+     if (llave == False):
+          print("carro apagado")
+
+def girarIzquierda():
+     print ("girar a la izquierda")
+
+def girarDerecha():
+     print ("girar a la derecha")
+
+###encender()
+apagar()
