87bdf6afe3ad88e24ca7e5262ffa8ac01da40e79
Nefta11 <neftaliarturohernandez@gmail.com>
2024-10-21 16:24:06 -0600

    add class bicicleta

---
+++ Practicas Python con RICHIE/Bicicleta.py
@@ -0,0 +1,19 @@
+class Bicicleta:
+    def __init__(self, tipo_sillon, num_rin, diam_rueda, tipo_color):
+        print(f"Creando una bicicleta {tipo_sillon} de {num_rin} rin, {diam_rueda} de diámetro y {tipo_color} color")
+
+    def frenar(self):
+        print("Frenando")
+
+    def girar(self):
+        print("La bicicleta está girando")
+
+    def pedalear(self):
+        print("Pedaleando")
+
+Baika = Bicicleta("Baika", "2", "26", "negro")
+
+Baika.pedalear()
+Baika.girar()
+Baika.frenar()
+
