be2183d7f7c8a6e5aa85aaa681ee29025174b519
Nefta11 <neftaliarturohernandez@gmail.com>
2024-10-21 16:09:43 -0600

    Add new atributte

--- Practicas Python con RICHIE/Ejercicio6POO.py
+++ Practicas Python con RICHIE/Ejercicio6POO.py
@@ -3,8 +3,9 @@ class Perro:
     def __init__(self,nombre,raza):
         print(f"Creando un perro {nombre} de raza {raza}")
     def ladrar(self):
-        print("Guau")
-    pass
+        print("Guau, guau, guau")
+    def caminar(self, pasos):
+        print(f"El perro realizo {pasos} pasos")
 
 mi_perro = Perro("Toby","Labrador")
 su_perro = Perro("Firula","Bulldog")
@@ -16,3 +17,7 @@ print(su_perro.especie)
 print(Perro.especie)
 
 mi_perro.ladrar()
+su_perro.ladrar()
+
+mi_perro.caminar(10)
+su_perro.caminar(20)
