866ef44428ca5eade27b5b3889c8e37504bd4a97
iMac 02 <imac02@iMacs-iMac03.local>
2024-10-22 15:47:03 -0600

    Updated code

--- Practicas Python con RICHIE/Bicicleta.py
+++ Practicas Python con RICHIE/Bicicleta.py
@@ -1,6 +1,6 @@
 class Bicicleta:
     def __init__(self, tipo_sillon, num_rin, diam_rueda, tipo_color):
-        print(f"Creando una bicicleta {tipo_sillon} de {num_rin} rin, {diam_rueda} de diámetro y {tipo_color} color")
+        print(f"Creando una bicicleta {tipo_sillon} de {num_rin} rin, {diam_rueda} de diámetro y  color {tipo_color}")
 
     def frenar(self):
         print("La bicicleta está frenando")
@@ -11,7 +11,7 @@ class Bicicleta:
     def pedalear(self):
         print("Se esta pedaleando")
 
-Baika = Bicicleta("Baika", "2", "26", "negro")
+Baika = Bicicleta("Patafuego", 2, 26, "Verde")
 
 Baika.pedalear()
 Baika.girar()
