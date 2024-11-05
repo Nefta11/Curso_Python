664821a00381ebb3b1200e2ac61bc66f22e55ed9
Nefta11 <neftaliarturohernandez@gmail.com>
2024-11-05 11:49:07 -0600

    Add practic of Qt Designer

--- Practicas Python con RICHIE/App/login.py
+++ Practicas Python con RICHIE/App/login.py
@@ -1,8 +1,16 @@
-from Frmlogin import *
+from PyQt5 import QtWidgets
+from FormLog import Ui_Dialog  # Importa la clase generada con el nombre correcto
 
-class MainWindow(QtWidgets.QMainWindow,Ui_MainWindow):
+class MainWindow(QtWidgets.QDialog, Ui_Dialog):  # Cambia Ui_MainWindow por Ui_Dialog
     def __init__(self, *args, **kwargs):
-        QtWidgets.QMainWindow.__init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.setupUi(self)
 
-        
+        # Modifica el texto del label_3 como tenías en tu código original
+        self.label.setText("Hola Soy Neftali")
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec_()
