a2b2e2342f345f8fd2fbc93f9318b60649f7136d
Nefta11 <neftaliarturohernandez@gmail.com>
2024-11-26 12:02:14 -0600

    Eliminar funcionalidades de desencriptación del menú, simplificando la interfaz de usuario y mejorando la navegación

--- Practicas Python con RICHIE/App/Menu.py
+++ Practicas Python con RICHIE/App/Menu.py
@@ -8,28 +8,15 @@ class Menu(QtWidgets.QMainWindow, Ui_FormMenu):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
-        self.pushButton.clicked.connect(self.show_encrypt)
-        self.pushButton2.clicked.connect(self.show_decrypt)
-        self.menuHolala.addAction("Encriptar", self.show_encrypt)
+        self.actionEncriptar.triggered.connect(self.show_encrypt)
 
     def show_encrypt(self):
         self.encrypt_window = Encrypt()
         self.encrypt_window.show()
         self.close()
 
-    def show_decrypt(self):
-        self.decrypt_window = Decrypt()
-        self.decrypt_window.show()
-        self.close()
-
 
 class Encrypt(QtWidgets.QMainWindow, Ui_SistemaEncriptar):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.setupUi(self)
-
-
-class Decrypt(QtWidgets.QMainWindow, Ui_SistemaEncriptar):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
