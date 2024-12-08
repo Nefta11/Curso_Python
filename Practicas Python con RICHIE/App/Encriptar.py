aac22fa5de51c7fc52ccc7b5b6a406a3721276cd
Nefta11 <neftaliarturohernandez@gmail.com>
2024-12-08 12:50:13 -0600

    Agregar funcionalidad para encriptar imágenes: implementar métodos para cargar, limpiar, encriptar y guardar imágenes en formato de texto.

--- Practicas Python con RICHIE/App/Encriptar.py
+++ Practicas Python con RICHIE/App/Encriptar.py
@@ -1,11 +1,13 @@
 from FormEncriptar import Ui_FrmEncriptar
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from PyQt5.QtWidgets import QMessageBox, QFileDialog, QMainWindow, QApplication
+from PyQt5 import QtGui
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
 from datetime import datetime
 import smtplib
 from email.message import EmailMessage
+import os
 
 class Encriptar(QMainWindow, Ui_FrmEncriptar):
     def __init__(self, *args, **kwargs):
@@ -15,6 +17,10 @@ class Encriptar(QMainWindow, Ui_FrmEncriptar):
         self.btnLimpiar.clicked.connect(self.limpiar)
         self.btnCargar.clicked.connect(self.uploadFichero)
         self.btnDescargar.clicked.connect(self.crearFichero)
+        self.btnEncriptar_2.clicked.connect(self.encrypt_image_AES)
+        self.btnLimpiar_2.clicked.connect(self.limpiar_imagen)
+        self.btnCargar_2.clicked.connect(self.upload_imagen)
+        self.btnDescargar_2.clicked.connect(self.crearFichero_imagen)
     
     def encrypt_data_AES(self):
         data = self.txtMensaje.toPlainText()
@@ -87,6 +93,42 @@ class Encriptar(QMainWindow, Ui_FrmEncriptar):
 
         # Close connection to server
         server.quit()
+
+    def encrypt_image_AES(self):
+        if hasattr(self, 'image_path'):
+            with open(self.image_path, 'rb') as image_file:
+                image_data = image_file.read()
+            key = b"123456789101112131415161718_UTXJ"
+            iv = b"TI_UTXJ2024ENCRI"
+            padder = padding.PKCS7(128).padder()
+            padded_data = padder.update(image_data)
+            padded_data += padder.finalize()
+            cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+            encryptor = cipher.encryptor()
+            ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+            self.label.setText(f'{ciphertext}')
+    
+    def limpiar_imagen(self):
+        self.label.clear()
+    
+    def crearFichero_imagen(self):
+        cadena = self.label.text()
+        if cadena != "":
+            fecha = datetime.now()
+            d = fecha.strftime("%m-%d-%Y%H-%M-%S")
+            with open(f'archivosEnc/ImagenEnc-{d}.txt', 'w') as fichero:
+                fichero.write(cadena)
+            QMessageBox.about(self, "Archivo", "Datos guardados correctamente")
+        else:
+            QMessageBox.about(self, "Error", "No hay imagen encriptada que almacenar")
+    
+    def upload_imagen(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "", "All Files (*.jpg *.png *.bmp);;Python Files (*.py)", options=options)
+        if fileName:
+            self.label.setPixmap(QtGui.QPixmap(fileName))
+            self.image_path = fileName  # Guardar la ruta del archivo de imagen
 
 if __name__ == "__main__":
     import sys
